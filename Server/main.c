9e800384722ede4e78a37da37709b1f3a8b55144
Hugo Ferreira <84676077+hooper1337@users.noreply.github.com>
2022-04-26 02:21:32 +0100

    Game Configuration
    Started Game configuration trough registry.

--- Server/main.c
+++ Server/main.c
@@ -4,11 +4,98 @@
 #include <stdio.h>	
 #include <stdlib.h>
 #include <tchar.h>
+#define BUFFER 256
+
+
+
+typedef struct _Game{
+	TCHAR* board;
+	DWORD rows;
+	DWORD columns;
+	DWORD time;
+}Game;
+
+typedef struct _Registry{
+	HKEY key;
+	TCHAR keyCompletePath[BUFFER];
+	DWORD dposition;
+	TCHAR name[BUFFER];
+}Registry;
 
 
 int _tmain(int argc, TCHAR** argv) {
+// Default code for windows32 API
+#ifdef UNICODE
+	_setmode(_fileno(stdin), _O_WTEXT);
+	_setmode(_fileno(stdout), _O_WTEXT);
+	_setmode(_fileno(stderr), _O_WTEXT);
+#endif
 
-	_tprintf(TEXT("IM THE CLIENT"));
+// Variables
+	Game game;
+	Registry registry;
+	_tcscpy_s(registry.keyCompletePath, BUFFER, TEXT("SOFTWARE\\PipeGame\0"));
+
+	if (argc != 4) {
+		if (RegCreateKeyEx(HKEY_CURRENT_USER, registry.keyCompletePath, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &registry.key, &registry.dposition) == ERROR_SUCCESS)
+		{
+			_tprintf_s(TEXT("\nI just created the key for the PipeGame!"));
+			DWORD rows = 10;
+			DWORD columns = 10;
+			DWORD time = 100;
+
+			long rowsSet = RegSetValueEx(registry.key, TEXT("Rows"), 0, REG_DWORD, (LPBYTE)&rows, sizeof(DWORD));
+			long columnsSet = RegSetValueEx(registry.key, TEXT("Columns"), 0, REG_DWORD, (LPBYTE)&columns, sizeof(DWORD));
+			long timeSet = RegSetValueEx(registry.key, TEXT("Time"), 0, REG_DWORD, (LPBYTE)&time, sizeof(DWORD));
+
+			if (rowsSet != ERROR_SUCCESS || columnsSet != ERROR_SUCCESS || timeSet != ERROR_SUCCESS) {
+				_tprintf_s(TEXT("\nCouldnt configure the values for the pipe game!"));
+				return -1;
+			}
+
+			long rowsRead = RegQueryValueEx(registry.key, TEXT("Rows"), NULL, REG_DWORD, (LPBYTE)&game.rows, sizeof(DWORD));
+			long columnsRead = RegQueryValueEx(registry.key, TEXT("Columns"), NULL, REG_DWORD, (LPBYTE)&game.columns, sizeof(DWORD));
+			long timeRead = RegQueryValueEx(registry.key, TEXT("Time"), NULL, REG_DWORD, (LPBYTE)&game.time, sizeof(DWORD));
+			
+			if (rowsRead != ERROR_SUCCESS || columnsRead != ERROR_SUCCESS || timeRead != ERROR_SUCCESS) {
+				_tprintf_s(TEXT("\nCouldnt read the values for the pipe game!"));
+				return -1;
+			}
+
+		}else {
+			_tprintf_s(TEXT("\nCouldnt create the desired key!"));
+			return -1;
+		}
+	}else {
+		game.rows = (DWORD) argv[1];
+		game.columns = (DWORD) argv[2];
+		game.time = (DWORD) argv[3];
+		_tprintf(TEXT("%d\n"), game.rows);
+		_tprintf(TEXT("%d\n"), game.columns);
+	}
+
+	game.board = (TCHAR*)malloc(game.rows * game.columns * sizeof(TCHAR));
+
+	if (game.board != NULL) {
+		_tprintf(TEXT("\nMemory alocation for the board done successfully\n"));
+	}
+
+	//for (DWORD i = 0; i < game.rows * game.columns; i++)
+			//game.board[i] = TEXT("_");
+
+	//for (DWORD i = 0; i < game.rows; i++)
+	//{
+		//_tprintf(TEXT("\n"));
+		//for (DWORD j = 0; j < game.columns; j++)
+			//_tprintf(TEXT("%c"), game.board[i * game.rows + j]);
+	//}
+
+
+
+
+	
+	
+	//RegCloseKey(registry.key);
 
 	return 0;
 }
