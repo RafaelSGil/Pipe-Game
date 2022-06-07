d562dfece481bafa2450b237cbc8dc657fa9abe1
Hugo Ferreira <84676077+hooper1337@users.noreply.github.com>
2022-06-07 14:56:06 +0100

    Redefinition of project

---
+++ src/Server/Game.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef GAME
+#include <Windows.h>
+typedef struct _Game {
+	TCHAR board[20][20];
+	DWORD rows;
+	DWORD columns;
+	DWORD begginingR;
+	DWORD begginingC;
+	DWORD endR;
+	DWORD endC;
+	TCHAR pieces[6];
+	DWORD shutdown;
+	BOOLEAN random;
+	DWORD index;
+	DWORD suspended;
+	TCHAR piece;
+	DWORD row;
+	DWORD column;
+	DWORD time;
+}Game;
+#endif // !GAME
---
+++ src/Server/Pipes.h
@@ -0,0 +1,21 @@
+
+#pragma once
+#ifndef PIPES
+#include <Windows.h>
+#include "Game.h"
+#define N 2
+#define PIPE_NAME TEXT("\\\\.\\pipe\\PipeGame")
+
+typedef struct {
+	HANDLE hInstancia;
+	OVERLAPPED overlap;
+	BOOL activo;
+}pipeData;
+
+typedef struct {
+	pipeData hPipes[N];
+	HANDLE hEvents[N];
+	HANDLE hMutex;
+	int terminar;
+}threadData;
+#endif 
